410498fccfc8d1c4d6a419c788d0b4d16be6fd1c
BrennaManning <brenna.manning@students.olin.edu>
2016-11-10 23:13:25 -0500

    tiny change vmax from 1 to 2

--- code/WorldViewer.py
+++ code/WorldViewer.py
@@ -4,7 +4,7 @@ from Cell2D import Cell2D, Cell2DViewer
 
 class WorldViewer(Cell2DViewer):
     """Generates visualization and animation of the world"""
-    vmax = 1
+    vmax = 2
     options = dict(interpolation='none', alpha=0.6, 
                    vmin=0, vmax=vmax)
    
