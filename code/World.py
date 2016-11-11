f1bbb61f4d9169b18b891825a4759026b3e598ea
kghite <kghite@gmail.com>
2016-11-11 00:13:45 -0500

    Debug World

--- code/World.py
+++ code/World.py
@@ -2,7 +2,7 @@
 World for sick and healthy agents
 """
 
-import Agent 
+from Agent import Agent
 import numpy as np
 import random
 from Cell2D import Cell2D
@@ -20,7 +20,9 @@ class World(Cell2D):
 		if agent_array == None:
 			# populate agent_array
 			self.agent_array = [[None for x in range(self.dim)] for y in range(self.dim)]
-			health_shuffle = np.zeros(self.dim**2 - self.num_sick) + [1 for _ in range(self.num_sick)]
+			sick_list = [1 for _ in range(self.num_sick)]
+			healthy_list = [0 for _ in range(self.dim**2 - self.num_sick)]
+			health_shuffle = sick_list + healthy_list
 			random.shuffle(health_shuffle)
 
 			for i in range(self.dim):
@@ -30,14 +32,14 @@ class World(Cell2D):
 			self.agent_array = agent_array
 
 		# Set up occupancy grid to keep track of agent presence
-		self.occupancy_grid = np.zeros(self.dim, self.dim)
+		self.occupancy_grid = np.zeros((self.dim, self.dim))
 		for x in range(self.dim-1):
 			for y in range(self.dim-1): 
 				if self.agent_array[i][j] != None:
 					self.occupancy_grid[i][j] = 1
 
 		# Create an agent health visualization array
-		self.array = np.zeros(self.dim, self.dim)
+		self.array = np.zeros((self.dim, self.dim))
 
 
 	# Every step represents the change in one hour
@@ -46,13 +48,13 @@ class World(Cell2D):
 		# Loop through agent objects and update based on params
 		for i in range(self.dim-1):
 			for j in range(self.dim-1):
-				if agent_array[i][j] != None:
+				if self.agent_array[i][j] != None:
 					# Get list of neighbors health
 					neighbors_health = []
 					for a in range(i - 1, i + 2):
 						for b in range(j - 1, j + 2):
 							if (a, b) != (i, j):
-								neighbors_health.append(agent_array[a][b].health)
+								neighbors_health.append(self.agent_array[a][b].health)
 
 					# Get fraction of neighbors that are sick
 					np.ceil(neighbors_health)
@@ -60,23 +62,23 @@ class World(Cell2D):
 
 					# Update agent health
 					# If sick (and contagious)
-					if agent_array[i][j].health == 1:
+					if self.agent_array[i][j].health == 1:
 						if random.randrange(0, 100) == 1:
-							agent_array[i][j].health = 0
+							self.agent_array[i][j].health = 0
 
 					# If health and contagious
-					elif agent_array[i][j].health < 1 and agent_array[i][j].health > 0:
-						agent_array[i][j].health += 0.1	
+					elif self.agent_array[i][j].health < 1 and self.agent_array[i][j].health > 0:
+						self.agent_array[i][j].health += 0.1	
 
 					# If healthy
 					else:
-						if random.randrange(neigh_health_frac*100, 100) > agent_array[i][j].immunity*100:
-							agent_array[i][j].health = 0.1
+						if random.randrange(int(neigh_health_frac*100), 100) > self.agent_array[i][j].immunity*100:
+							self.agent_array[i][j].health = 0.1
 
-		udate_vis(self.agent_array)
+		self.update_vis()
 
 
-	def update_vis(self, agents):
+	def update_vis(self):
 		for i in range(self.dim-1):
 			for j in range(self.dim-1):
-				self.array[i][j] = self.agent[i][j].health + 1
+				self.array[i][j] = self.agent_array[i][j].health + 1
