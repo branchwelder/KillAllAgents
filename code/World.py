b113526b449c20f178644c4a6ca8316dccf8fb1a
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-11-13 11:11:46 -0500

    added code for graphing and animation

--- code/World.py
+++ code/World.py
@@ -1,7 +1,3 @@
-"""
-World for sick and healthy agents
-"""
-
 from Agent import Agent
 import numpy as np
 import random
@@ -10,42 +6,53 @@ import math
 from copy import deepcopy
 
 class World(Cell2D):
+	"""
+	Represents a world of sick and healthy agents.
+	"""
 
 	def __init__(self, agent_array=None, **params):
 
 		# Set up world params
-		self.dim = params.get("n", 30)
-		self.num_population = params.get("num_agents", self.dim**2)
+		self.n = params.get("n", 10)
+		self.num_population = params.get("num_agents", self.n**2)
 		self.num_sick = params.get("num_sick", 1)
+
+		# Initialize arrays to track array data
+		self.healthy = []
+		self.contagious = []
+		self.sick = []
 
 		# Set up the agent population
 		if agent_array == None:
 			# populate agent_array
-			self.agent_array = [[None for x in range(self.dim)] for y in range(self.dim)]
+
+			self.agent_array = [[None for x in range(self.n)] for y in range(self.n)]
+
 			sick_list = [1 for _ in range(self.num_sick)]
-			healthy_list = [0 for _ in range(self.dim**2 - self.num_sick)]
+			healthy_list = [0 for _ in range(self.n**2 - self.num_sick)]
 			health_shuffle = sick_list + healthy_list
 			random.shuffle(health_shuffle)
 
-			for i in range(self.dim):
-				for j in range(self.dim):
-					self.agent_array[i][j] = Agent(health=health_shuffle[self.dim * i + j])
+			for i in range(self.n):
+				for j in range(self.n):
+					self.agent_array[i][j] = Agent(health=health_shuffle[self.n * i + j])
 		else:
 			self.agent_array = agent_array
 
 		# Set up occupancy grid to keep track of agent presence
-		self.occupancy_grid = np.zeros((self.dim, self.dim))
-		for x in range(self.dim):
-			for y in range(self.dim):
+		self.occupancy_grid = np.zeros((self.n, self.n))
+		for x in range(self.n):
+			for y in range(self.n):
 				if self.agent_array[i][j] != None:
 					self.occupancy_grid[i][j] = 1
 
 		# Create an agent health visualization array
-		self.array = np.zeros((self.dim, self.dim))
-		for i in range(self.dim):
-			for j in range(self.dim):
+		self.array = np.zeros((self.n, self.n))
+		for i in range(self.n):
+			for j in range(self.n):
 				self.array[i][j] = self.agent_array[i][j].health + 1
 		#print(self.array)
+
 
 
 	# Every step represents the change in one hour
@@ -54,8 +61,8 @@ class World(Cell2D):
 		# Loop through agent objects and update based on params
 		new_agent_array = deepcopy(self.agent_array)
 
-		for i in range(self.dim):
-			for j in range(self.dim):
+		for i in range(self.n):
+			for j in range(self.n):
 				if self.agent_array[i][j] != None:
 					#print("Cell: " + str((i, j)))
 					# Get list of neighbors health
@@ -63,7 +70,7 @@ class World(Cell2D):
 					for a in range(i - 1, i + 2):
 						for b in range(j - 1, j + 2):
 							if (a, b) != (i, j):
-								if a >= 0 and a < self.dim and b >= 0 and b < self.dim:
+								if a >= 0 and a < self.n and b >= 0 and b < self.n:
 									neighbors_health.append(self.agent_array[a][b].health)
 
 					# Get fraction of neighbors that are sick
@@ -84,27 +91,26 @@ class World(Cell2D):
 
 					# If healthy
 					elif self.agent_array[i][j].health == 0:
-						#print("I was healthy")
 						immunity = self.agent_array[i][j].immunity*100
 						neigh_health_chance = math.floor(neigh_health_frac*100)
 						health_chance = random.randrange(neigh_health_chance, 102)
-						##print("Immunity: " + str(immunity))
-						#print("Neighbor Health Input: " + str(neigh_health_chance))
-						#print("Health Chance: " + str(health_chance))
 
 						if neigh_health_chance == 0:
-							#print("I stayed healthy")
 							new_agent_array[i][j].health = 0
 						elif health_chance > immunity:
-							#print("I became contagious")
 							new_agent_array[i][j].health = 0.1
 
 		self.agent_array = deepcopy(new_agent_array)
 		self.update_vis()
+		return np.count_nonzero(self.array > 1)
 
 
 	def update_vis(self):
-		for i in range(self.dim):
-			for j in range(self.dim):
+		for i in range(self.n):
+			for j in range(self.n):
 				self.array[i][j] = deepcopy(self.agent_array[i][j].health) + 1
-		#print(self.array)
+
+		# track numbers for each step
+		self.healthy.append(np.count_nonzero(self.array == 1))
+		self.contagious.append(np.count_nonzero((self.array > 1) & (self.array < 2)))
+		self.sick.append(np.count_nonzero(self.array == 2))
--- code/run_experiment.py
+++ code/run_experiment.py
@@ -1,14 +1,57 @@
 from World import World
 from WorldViewer import WorldViewer
 
+import thinkplot
+import matplotlib.pyplot as plt
 
-def make_animation(path, frames, fps):
-    world = World()
+
+def sweep_immunity(path, n, frames):
+    for immunity in [0.3, 0.5, 0.8, 0.99]:
+        grid = World(n=n, immunity=immunity)
+        segs = [grid.step() for i in range(frames)]
+        thinkplot.plot(segs, label='immunity = %.1f' % immunity)
+
+    thinkplot.config(xlabel='Time steps', ylabel='num_sick + num_contagious',
+                    loc='lower right')
+
+    plt.savefig(path+'immunity_sweep.pdf')
+    plt.clf()
+    print("Immunity sweep saved!")
+
+
+def graph_bins(path, n, frames):
+    world = World(n=n)
+
+    for i in range(frames):
+        world.step()
+
+    thinkplot.plot(world.healthy, label='healthy')
+    thinkplot.plot(world.sick, label='sick')
+    thinkplot.plot(world.contagious, label='contagious')
+
+    thinkplot.config(xlabel='Time steps', ylabel='num_agents',
+                    loc='lower right')
+
+    plt.savefig(path+'bin_graph.pdf')
+    plt.clf()
+    print("Bin graph saved!")
+
+
+def run_experiment(path, n, frames, fps):
+    world = World(n=n)
     viewer = WorldViewer(world)
-    anim = viewer.animate(frames=frames)
 
-    anim.save(path, fps=fps, extra_args=['-vcodec', 'libx264'])
+    # Create and save animation
+    anim = viewer.animate(frames=frames)
+    anim.save(path+"animation.mp4", fps=fps, extra_args=['-vcodec', 'libx264'])
+    plt.clf()
+    print("Animation saved!")
+
+    # Make graphs`
+    graph_bins(path, n, frames)
+    sweep_immunity(path, n, frames)
+
 
 
 if __name__ == "__main__":
-    make_animation("output/basic_animation.mp4", 100, 5)
+    run_experiment("output/", n=100, frames=200, fps=5)
